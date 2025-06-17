d14ec2282f446c1700b726edca53e4a2ce721f75
brianxia273 <briangxia@gmail.com>
2025-06-16 22:59:55 -0400

    6/16 - 25-40 Data 1 Film

--- Models/All-DataGenerate.py
+++ Models/All-DataGenerate.py
@@ -1,7 +1,7 @@
 # Runner script for DataGenerate scripts of SVR, BRR, and GPR regression models
 import subprocess
 
-print("Starting all DataGenerate")
+print("Starting all DataGenerate; DO NOT ADJUST CONFIG P1 WHILE RUNNING")
 subprocess.run(["python", "SVR-DataGenerate-MetaTrain.py"])
 subprocess.run(["python", "SVR-DataGenerate-PreTrain.py"])
 subprocess.run(["python", "BRR-DataGenerate.py"])
--- Models/config.py
+++ Models/config.py
@@ -5,10 +5,10 @@
 
 # Phase 1: WriteMetrics, DataGenerate, and GridSearch Configuration
 # NOTE: NEED TO SELF-MODIFY HYPERPARAMETERS FOR GPR AND SVR IN WriteMetrics AND DataGenerate
-p1Size: int = 35
+p1Size: int = 25
 p1Data: str = "Nitride (Dataset 1) NTi.csv"
 p1RandomState: int = 47
-p1YIndex: int = -1  # (-2) = film-thickness, (-1) = N/Ti ratio
+p1YIndex: int = -2  # (-2) = film-thickness, (-1) = N/Ti ratio
 
 p1SvrExtrapolationRange: float = 0.03
 p1N: int = 25600  # N = Augmented Data Count, {6400, 12800, 25600}
@@ -17,7 +17,7 @@ p1N: int = 25600  # N = Augmented Data Count, {6400, 12800, 25600}
 # ================================================================================
 
 # Phase 2: FCNN/1D-Conv PreTrain Configuration
-p2Size: int = 35
+p2Size: int = 25
 p2Data: str = "Nitride (Dataset 1) NTi.csv"
 p2RandomState: int = 47  # Selecting randomState of SVG augmented data
 p2YIndex: int = -2  # (-2) = film-thickness, (-1) = N/Ti ratio
@@ -37,7 +37,7 @@ p3MetaBatchSize: int = 20  # {5, 20}
 p3InnerStepSize: float = 0.05  # NEED TO DOUBLE-CHECK
 
 # Choosing Pre-Trained NN using its parameters
-p3NN: str = "FCNN" # 1D-Conv or FCNN
+p3NN: str = "1D-Conv" # 1D-Conv or FCNN
 p3NNSize = p3Size = 40
 p3NNEpoch: int = 1000  # {20, 200, 1000}
 p3NNBatch: int = 1028  # {16, 512, 1028}
@@ -59,37 +59,37 @@ p3seed: int = 42
 # Phase 4: NN FineTune Configuration
 
 p4LearningRate: float = 0.2  # {0.05, 0.2} - Is same as MetaLearn (?)
-p4Epochs: int = 200  # {5, 10, 100, 200} - Is same as MetaLearn (?)
-p4YIndex: int = -1  # (-2) = film-thickness, (-1) = N/Ti ratio
-p4Data: str = "NitrideMetal (Dataset 2) NTi.csv"
+p4Epochs: int = 1000  # {5, 10, 100, 200} - Is same as MetaLearn (?)
+p4YIndex: int = -2  # (-2) = film-thickness, (-1) = N/Ti ratio
+p4Data: str = "Nitride (Dataset 1) NTi.csv"
 p4BatchSize: int = 1028 # {16, 512, 1028} (?)
 
 # Choosing Meta-Trained NN using its parameters
-p4NN: str = "1D-Conv" # 1D-Conv or FCNN
-p4NNSize: int = 40
+p4NN: str = "FCNN" # 1D-Conv or FCNN
+p4NNSize: int = 35
 p4NNEpoch: int = 1000  # {20, 200, 1000}
 p4NNBatch: int = 1028  # {16, 512, 1028}
 p4N: int = 25600  # N {6400, 12800, 25600}
 
 # Choosing randomState for train/test, must be same as previous phases
-p4RandomState: int = 42
+p4RandomState: int = 47
 
 # ================================================================================
 # ================================================================================
 
 # Phase 5: Neural Network TestAccuracy Configuration
 
-p5Data: str = "NitrideMetal (Dataset 2) NTi.csv"
-p5YIndex: int = -1 # (-2) = film-thickness, (-1) = N/Ti ratio
+p5Data: str = "Nitride (Dataset 1) NTi.csv"
+p5YIndex: int = -2 # (-2) = film-thickness, (-1) = N/Ti ratio
 
 # Choosing randomState for same train/test, must be same as previous phases
-p5RandomState: int = 42
+p5RandomState: int = 47
 
 # Choosing Fine-Tuned NN using its parameters
 p5NN: str = "1D-Conv" # 1D-Conv or FCNN
-p5NNSize: int = 40
+p5NNSize: int = 25
 p5N: int = 25600  # N {6400, 12800, 25600}
-p5NNEpoch: int = 200  # {5, 10, 100, 200}
+p5NNEpoch: int = 1000  # {5, 10, 100, 200}
 p5NNBatch: int = 1028  # {16, 512, 1028}
 
 # ================================================================================
